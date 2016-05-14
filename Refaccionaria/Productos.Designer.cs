de19256b690b36b79817e10ce26cde790e8008f4
demg <demg@DESKTOP-7HFQG9F>
2016-05-13 21:05:44 -0500

    -Añadido el form de "Producto" el cual funciona al 100%. -los form "Ventas e Historia" no tienen codigo.

--- Refaccionaria/Productos.Designer.cs
+++ Refaccionaria/Productos.Designer.cs
@@ -29,10 +29,257 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.productoBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.refaccionariaDataSet2 = new Refaccionaria.refaccionariaDataSet2();
+            this.productoTableAdapter = new Refaccionaria.refaccionariaDataSet2TableAdapters.productoTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.productoBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.idproductoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nombreProductoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descripcionProductoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cantidadProductoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.precioProductoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productoBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.refaccionariaDataSet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productoBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idproductoDataGridViewTextBoxColumn,
+            this.nombreProductoDataGridViewTextBoxColumn,
+            this.descripcionProductoDataGridViewTextBoxColumn,
+            this.cantidadProductoDataGridViewTextBoxColumn,
+            this.precioProductoDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.productoBindingSource1;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(472, 132);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellLeave += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellLeave);
+            // 
+            // productoBindingSource
+            // 
+            this.productoBindingSource.DataMember = "producto";
+            this.productoBindingSource.DataSource = this.refaccionariaDataSet2;
+            // 
+            // refaccionariaDataSet2
+            // 
+            this.refaccionariaDataSet2.DataSetName = "refaccionariaDataSet2";
+            this.refaccionariaDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productoTableAdapter
+            // 
+            this.productoTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 169);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nombre:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 166);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(139, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Descripción:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 196);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(139, 67);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cantidad:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(303, 166);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(257, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Precio:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(304, 196);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(119, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(230, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Añadir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(311, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Editar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(393, 238);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Eliminar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // productoBindingSource1
+            // 
+            this.productoBindingSource1.DataMember = "producto";
+            this.productoBindingSource1.DataSource = this.refaccionariaDataSet2;
+            // 
+            // idproductoDataGridViewTextBoxColumn
+            // 
+            this.idproductoDataGridViewTextBoxColumn.DataPropertyName = "idproducto";
+            this.idproductoDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idproductoDataGridViewTextBoxColumn.Name = "idproductoDataGridViewTextBoxColumn";
+            this.idproductoDataGridViewTextBoxColumn.ReadOnly = true;
+            this.idproductoDataGridViewTextBoxColumn.Width = 50;
+            // 
+            // nombreProductoDataGridViewTextBoxColumn
+            // 
+            this.nombreProductoDataGridViewTextBoxColumn.DataPropertyName = "nombreProducto";
+            this.nombreProductoDataGridViewTextBoxColumn.HeaderText = "Nombre";
+            this.nombreProductoDataGridViewTextBoxColumn.Name = "nombreProductoDataGridViewTextBoxColumn";
+            this.nombreProductoDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // descripcionProductoDataGridViewTextBoxColumn
+            // 
+            this.descripcionProductoDataGridViewTextBoxColumn.DataPropertyName = "descripcionProducto";
+            this.descripcionProductoDataGridViewTextBoxColumn.HeaderText = "Descripción";
+            this.descripcionProductoDataGridViewTextBoxColumn.Name = "descripcionProductoDataGridViewTextBoxColumn";
+            this.descripcionProductoDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // cantidadProductoDataGridViewTextBoxColumn
+            // 
+            this.cantidadProductoDataGridViewTextBoxColumn.DataPropertyName = "cantidadProducto";
+            this.cantidadProductoDataGridViewTextBoxColumn.HeaderText = "Cantidad";
+            this.cantidadProductoDataGridViewTextBoxColumn.Name = "cantidadProductoDataGridViewTextBoxColumn";
+            this.cantidadProductoDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // precioProductoDataGridViewTextBoxColumn
+            // 
+            this.precioProductoDataGridViewTextBoxColumn.DataPropertyName = "precioProducto";
+            this.precioProductoDataGridViewTextBoxColumn.HeaderText = "Precio";
+            this.precioProductoDataGridViewTextBoxColumn.Name = "precioProductoDataGridViewTextBoxColumn";
+            this.precioProductoDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // Productos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Productos";
+            this.ClientSize = new System.Drawing.Size(497, 275);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Productos";
+            this.Text = "Productos - Refaccionaria";
+            this.Load += new System.EventHandler(this.Productos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productoBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.refaccionariaDataSet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productoBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private refaccionariaDataSet2 refaccionariaDataSet2;
+        private System.Windows.Forms.BindingSource productoBindingSource;
+        private refaccionariaDataSet2TableAdapters.productoTableAdapter productoTableAdapter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.BindingSource productoBindingSource1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idproductoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nombreProductoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descripcionProductoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cantidadProductoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn precioProductoDataGridViewTextBoxColumn;
     }
 }
--- Refaccionaria/Productos.cs
+++ Refaccionaria/Productos.cs
@@ -16,5 +16,101 @@ namespace Refaccionaria
         {
             InitializeComponent();
         }
+
+        private void Productos_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'refaccionariaDataSet2.producto' table. You can move, or remove it, as needed.
+            this.productoTableAdapter.Fill(this.refaccionariaDataSet2.producto);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            numericUpDown1.Value = Convert.ToDecimal(dataGridView1.CurrentRow.Cells[3].Value.ToString());
+            textBox3.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+
+            dataGridView1.CurrentRow.Cells[1].Style.BackColor = Color.Beige;
+            dataGridView1.CurrentRow.Cells[2].Style.BackColor = Color.Beige;
+            dataGridView1.CurrentRow.Cells[3].Style.BackColor = Color.Beige;
+            dataGridView1.CurrentRow.Cells[4].Style.BackColor = Color.Beige;
+        }
+
+        private void dataGridView1_CellLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.Rows[e.RowIndex].Cells[1].Style.BackColor = Color.White;
+            dataGridView1.Rows[e.RowIndex].Cells[2].Style.BackColor = Color.White;
+            dataGridView1.Rows[e.RowIndex].Cells[3].Style.BackColor = Color.White;
+            dataGridView1.Rows[e.RowIndex].Cells[4].Style.BackColor = Color.White;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // probar conexion
+            manejadorBD mbd = new manejadorBD("localhost", "refaccionaria");
+            //configuramos la instruccion
+            string instruccion = "INSERT INTO producto(nombreProducto,descripcionProducto,cantidadProducto,precioProducto) VALUES('{0}','{1}',{2},{3});";
+            instruccion = string.Format(instruccion, textBox1.Text, textBox2.Text, numericUpDown1.Value, Convert.ToDouble(textBox3.Text));
+            //ejecutamos la instruccion
+            int res = mbd.ejecutar(instruccion);
+            // validar si se realizo la operacion
+            if (res > 0)
+            {
+                MessageBox.Show("Producto Añadido Correctamente");
+                this.productoTableAdapter.Fill(this.refaccionariaDataSet2.producto);
+            }
+            else
+            {
+                MessageBox.Show("Producto NO Añadido Correctamente");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // probar conexion
+            manejadorBD mbd = new manejadorBD("localhost", "refaccionaria");
+            //configuramos la instruccion
+            string instruccion = "UPDATE producto SET nombreProducto='{0}', descripcionProducto='{1}', cantidadProducto={2}, precioProducto={3} WHERE idProducto={4};";
+            instruccion = string.Format(instruccion, textBox1.Text, textBox2.Text, numericUpDown1.Value, Convert.ToDouble(textBox3.Text), dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            //ejecutamos la instruccion
+            int res = mbd.ejecutar(instruccion);
+            // validar si se realizo la operacion
+            if (res > 0)
+            {
+                MessageBox.Show("Producto Editado Correctamente");
+                this.productoTableAdapter.Fill(this.refaccionariaDataSet2.producto);
+            }
+            else
+            {
+                MessageBox.Show("Producto NO Editado Correctamente");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // probar conexion
+            manejadorBD mbd = new manejadorBD("localhost", "refaccionaria");
+            //configuramos la instruccion
+            string instruccion = "DELETE FROM producto WHERE idProducto ={0};";
+            instruccion = string.Format(instruccion, dataGridView1.CurrentRow.Cells[0].Value.ToString());
+            //ejecutamos la instruccion
+            int res = mbd.ejecutar(instruccion);
+            // validar si se realizo la operacion
+            if (res > 0)
+            {
+                MessageBox.Show("Producto Eliminado Correctamente");
+                this.productoTableAdapter.Fill(this.refaccionariaDataSet2.producto);
+            }
+            else
+            {
+                MessageBox.Show("Producto NO Eliminado Correctamente");
+            }
+        }
     }
 }
